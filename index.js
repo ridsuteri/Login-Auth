774244c9de907b4678fb83f3c38255b90412345d
Riddhi Suteri <ridsuteri@gmail.com>
2021-11-28 10:40:04 +0530

    minor fix

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const passport = require('passport');
 const cookieSession = require('cookie-session');
 require('./passport-setup');
 
-const PORT = 5000;
+const PORT = 5000 || process.env.PORT;
 const app = express();
 
 app.use(cookieSession({
